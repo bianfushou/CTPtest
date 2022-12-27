eadf1f0e701cabecc01e2b101ee5f0c57aed0d5d
bianfushou <1029855298@qq.com>
2022-12-27 23:33:40 +0800

    add test

---
+++ src/test/TestStrategy.cpp
@@ -0,0 +1,273 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include "TestStrategy.h"
+
+extern std::unordered_map<std::string, TickToKlineHelper> g_KlineHash;
+
+
+void PivotReversalStrategy::operator()()
+{
+	if (!opStart) {
+		opStart = true;
+		curVolume = 0;
+	}
+	if (status >= 8) {
+		return;
+	}
+	else {
+		if (taskQue.size() > 0) {
+			taskQue.front()();
+			taskQue.pop_front();
+		}
+	}
+	std::lock_guard<std::mutex> lk(strategyMutex);
+	double swh = pivot(Strategy::Type::high);
+	double swl = pivot(Strategy::Type::low);
+	if (swh <= 0.0) {
+		if (highPivotQue.empty()) {
+			return;
+		}
+		swh = highPivotQue.back();
+	}
+
+	if (swl <= 0.0) {
+		if (lowPivotQue.empty()) {
+			return;
+		}
+		swl = lowPivotQue.back();
+	}
+	TickToKlineHelper& tickToKlineObject = g_KlineHash.at(instrumentID);
+	if (tickToKlineObject.lastPrice > swh) {
+		if (status == 0) {
+			this->preStatus = 0;
+			makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_Open, volume);
+
+			this->status = 8 | 1;
+		}
+		else if (status == 2) {
+			preStatus = 2;
+			{
+				makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Sell, THOST_FTDC_OF_CloseToday, curVolume.load());
+				this->status = 8;
+			}
+			auto lastPrice = tickToKlineObject.lastPrice;
+			taskQue.emplace_back([this, swh]() {
+				this->preStatus = 0;
+				std::lock_guard<std::mutex> lk(strategyMutex);
+				TickToKlineHelper& tickToKlineObject = g_KlineHash.at(this->instrumentID);
+				if (tickToKlineObject.lastPrice > swh) {
+					makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_Open, volume);
+					this->status = 8 | 1;
+				}
+				else {
+					this->status = 0;
+				}
+
+			});
+
+		}
+		else if (status == 1 && curVolume < volume) {
+			this->preStatus = 1;
+			makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_Open, volume - curVolume);
+			this->status = 8 | 1;
+		}
+	}
+	if (tickToKlineObject.lastPrice < swl) {
+		if (status == 0) {
+			preStatus = 0;
+			makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Sell, THOST_FTDC_OF_Open, volume);
+			status = 8 | 2;
+		}
+		else if (status == 1) {
+			this->preStatus = 1;
+			{
+				makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_CloseToday, curVolume.load());
+				this->status = 8;
+			}
+			auto lastPrice = tickToKlineObject.lastPrice;
+			taskQue.emplace_back([this, swl]() {
+				std::lock_guard<std::mutex> lk(strategyMutex);
+				this->preStatus = 0;
+				TickToKlineHelper& tickToKlineObject = g_KlineHash.at(this->instrumentID);
+				if (tickToKlineObject.lastPrice < swl) {
+					makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Sell, THOST_FTDC_OF_Open, volume);
+					this->status = 8 | 2;
+				}
+				else {
+					this->status = 0;
+				}
+			});
+
+		}
+		else if (status == 2 && curVolume < volume) {
+			this->preStatus = 2;
+			this->status = 8 | 2;
+			makeOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_Open, volume - curVolume);
+		}
+	}
+}
+
+void PivotReversalStrategy::clearInvestor(CThostFtdcInvestorPositionField investor, int status, bool isLast) {
+	if (status <= 0) {
+		return;
+	}
+	tasks.emplace_back([this, investor, status, isLast]() {
+
+		int limit = instrumentField.MaxMarketOrderVolume / 3;
+		limit = limit > instrumentField.MinMarketOrderVolume ? limit : instrumentField.MinMarketOrderVolume;
+		std::lock_guard<std::mutex> lk(strategyMutex);
+
+		TickToKlineHelper& tickToKlineObject = g_KlineHash.at(instrumentID);
+		if ((status & 1) != 0) {
+			int YdPosition = investor.Position - investor.TodayPosition;
+			if (investor.PosiDirection == THOST_FTDC_PD_Long) {
+				this->longInvestor = investor;
+				if (instrumentField.MaxMarketOrderVolume < YdPosition) {
+					for (int p = YdPosition; p > 0; p -= limit) {
+						std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+						makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_Close, p > limit ? limit : p);
+					}
+				}
+				else if (YdPosition > 0) {
+					makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_Close, YdPosition);
+				}
+			}
+			else if (investor.PosiDirection == THOST_FTDC_PD_Short) {
+				if (instrumentField.MaxMarketOrderVolume < YdPosition) {
+					for (int p = YdPosition; p > 0; p -= limit) {
+						std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+						makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Sell, THOST_FTDC_OF_Close, p > limit ? limit : p);
+					}
+				}
+				else if (YdPosition > 0) {
+					makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Sell, THOST_FTDC_OF_Close, YdPosition);
+				}
+
+			}
+		}
+		if ((status & 2) != 0) {
+			if (investor.PosiDirection == THOST_FTDC_PD_Long) {
+				this->longInvestor = investor;
+				if (instrumentField.MaxMarketOrderVolume < investor.TodayPosition) {
+					for (int p = investor.TodayPosition; p > 0; p -= limit) {
+						std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+						makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_CloseToday, p > limit ? limit : p);
+					}
+
+				}
+				else if (investor.TodayPosition > 0) {
+					makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Buy, THOST_FTDC_OF_CloseToday, investor.TodayPosition);
+				}
+			}
+			else if (investor.PosiDirection == THOST_FTDC_PD_Short) {
+				shortInvestor = investor;
+				if (instrumentField.MaxMarketOrderVolume < investor.TodayPosition) {
+					for (int p = investor.TodayPosition; p > 0; p -= limit) {
+						std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+						makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Sell, THOST_FTDC_OF_CloseToday, p > limit ? limit : p);
+					}
+				}
+				else if (investor.TodayPosition > 0) {
+					makeClearOrder(tickToKlineObject.lastPrice, THOST_FTDC_D_Sell, THOST_FTDC_OF_CloseToday, investor.TodayPosition);
+				}
+
+			}
+		}
+
+		if (!last) {
+			initVolume = initVolume + investor.Position + investor.YdPosition;
+		}
+		if (isLast) {
+			last = true;
+		}
+		MvStatus = 1;
+	});
+}
+
+double PivotReversalStrategy::pivot(Strategy::Type type) {
+	int range = left + right;
+	std::vector<double> pivotArray;
+	TickToKlineHelper& tickToKlineObject = g_KlineHash.at(instrumentID);
+	if (tickToKlineObject.m_KLineDataArray.size() < range) {
+		return 0.0;
+	}
+
+	size_t size = tickToKlineObject.m_KLineDataArray.size();
+	bool isMin = false;
+	switch (type) {
+	case Strategy::Type::high:
+	{
+		if (size == barsNumHigh) {
+			return 0.0;
+		}
+		else {
+			barsNumHigh = size;
+		}
+		int i = range;
+		for (auto it = tickToKlineObject.m_KLineDataArray.rbegin(); i > 0; i--, it++) {
+			pivotArray.push_back(it->high_price);
+		}
+		break;
+	}
+	case Strategy::Type::low:
+	{
+		if (size == barsNumLow) {
+			return 0.0;
+		}
+		else {
+			barsNumLow = size;
+		}
+		int i = range;
+		for (auto it = tickToKlineObject.m_KLineDataArray.rbegin(); i > 0; i--, it++) {
+			pivotArray.push_back(it->low_price);
+		}
+		isMin = true;
+		break;
+	}
+	case Strategy::Type::open:
+		break;
+	case Strategy::Type::close:
+		break;
+	}
+	double pivotVal = pivotArray[right];
+	for (int i = 0; i < range; i++) {
+		if (i < right) {
+			if (isMin) {
+				if (pivotVal >= pivotArray[i] || fabs(pivotVal - pivotArray[i]) < 0.0005) {
+					return 0.0;
+				}
+			}
+			else {
+				if (pivotVal <= pivotArray[i] || fabs(pivotVal - pivotArray[i]) < 0.0005) {
+					return 0.0;
+				}
+			}
+		}
+		else if (i > right) {
+			if (isMin) {
+				if (pivotVal > pivotArray[i]) {
+					return 0.0;
+				}
+			}
+			else {
+				if (pivotVal < pivotArray[i]) {
+					return 0.0;
+				}
+			}
+		}
+	}
+	if (!isMin) {
+		highPivotQue.push_back(pivotVal);
+		outFile << "high:" << pivotVal << std::endl;
+	}
+	else {
+		lowPivotQue.push_back(pivotVal);
+		outFile << "low:" << pivotVal << std::endl;
+	}
+
+	return pivotVal;
+}
---
+++ src/test/TestStrategy.h
@@ -0,0 +1,282 @@
+#pragma once
+// ---- 简单策略交易的类 ---- //
+
+#include"../Config.h"
+#include <functional>
+#ifdef CTPTest
+#include "../CTPTest_API/ThostFtdcUserApiStruct.h"
+#else
+#include "../CTP_API/ThostFtdcUserApiStruct.h"
+#endif
+#include "../TickToKlineHelper.h"
+#include <list>
+#include <fstream>
+#include <mutex>
+#include <atomic>
+
+class Strategy {
+public:
+	enum Type {
+		open, high, low, close
+	};
+
+	void setInstrument(TThostFtdcInstrumentIDType instrumentId) {
+		instrumentID = std::string(instrumentId);
+	}
+
+	void setVolume(TThostFtdcVolumeType volume) {
+		this->volume = volume;
+	}
+
+	virtual void init() = 0;
+
+	virtual void operator()() = 0;
+
+	void start() {
+		tradeStart = true;
+	}
+
+	bool getTradeStart() {
+		return tradeStart;
+	}
+protected:
+	std::string instrumentID;
+	TThostFtdcVolumeType volume = 1;
+	bool tradeStart = false;
+};
+
+
+class PivotReversalStrategy : public Strategy {
+public:
+	enum Type {
+		open, high, low, close
+	};
+
+	void setLRBars(int left, int right) {
+		this->left = left;
+		this->right = right;
+	}
+	virtual void init() override {
+		outFile.open(instrumentID + "_Strategy.txt");
+		CommissionFile.open(instrumentID + "_Commission.csv");
+		CommissionFile << "合约代码" << ","
+			<< "手数" << ","
+			<< "价格" << ","
+			<< "手续费" << ","
+			<< "开平仓标志" << ","
+			<< "开平仓方向" << ","
+			<< "盈利金额"
+			<< std::endl;
+		winFile.open(instrumentID + "_winRate.csv");
+		winFile << "win"
+			<< "," << "盈亏比" << std::endl;
+	}
+	virtual void operator()() override;
+
+	void resetStatus() {
+		std::lock_guard<std::mutex> lk(strategyMutex);
+		taskQue.clear();
+		status = preStatus;
+	}
+	void statusDone() {
+		std::lock_guard<std::mutex> lk(strategyMutex);
+		if (status >= 8) {
+			status -= 8;
+		}
+	}
+
+	int getStatus() {
+		return status;
+	}
+
+	void makeOrder(double lastPrice, TThostFtdcDirectionType direction, TThostFtdcOffsetFlagType offsetFlag, TThostFtdcVolumeType volume) {
+		std::shared_ptr<CThostFtdcInputOrderField> orderInsertReq = std::make_shared<CThostFtdcInputOrderField>();
+		memset(orderInsertReq.get(), 0, sizeof(CThostFtdcInputOrderField));
+		strcpy(orderInsertReq->InstrumentID, this->instrumentID.c_str());
+		orderInsertReq->Direction = direction;
+		orderInsertReq->CombOffsetFlag[0] = offsetFlag;
+		orderInsertReq->LimitPrice = lastPrice;
+		orderInsertReq->VolumeTotalOriginal = volume;
+		orderInsertReq->StopPrice = 0;
+	}
+
+	void makeClearOrder(double lastPrice, TThostFtdcDirectionType direction, TThostFtdcOffsetFlagType offsetFlag, TThostFtdcVolumeType volume) {
+		std::shared_ptr<CThostFtdcInputOrderField> orderInsertReq = std::make_shared<CThostFtdcInputOrderField>();
+		memset(orderInsertReq.get(), 0, sizeof(CThostFtdcInputOrderField));
+		strcpy(orderInsertReq->InstrumentID, this->instrumentID.c_str());
+		orderInsertReq->Direction = direction;
+		orderInsertReq->CombOffsetFlag[0] = offsetFlag;
+		orderInsertReq->LimitPrice = 0;
+		orderInsertReq->VolumeTotalOriginal = volume;
+		//orderInsertReq->StopPrice = 0;
+		///报单引用
+		//strcpy(orderInsertReq.OrderRef, order_ref);
+		///报单价格条件: 限价
+		orderInsertReq->OrderPriceType = THOST_FTDC_OPT_AnyPrice;
+		///组合投机套保标志
+		orderInsertReq->CombHedgeFlag[0] = THOST_FTDC_HF_Speculation;
+		///有效期类型: 当日有效
+		orderInsertReq->TimeCondition = THOST_FTDC_TC_IOC;
+		///成交量类型: 任何数量
+		orderInsertReq->VolumeCondition = THOST_FTDC_VC_AV;
+		///最小成交量: 1
+		orderInsertReq->MinVolume = 1;
+		///触发条件: 立即
+		orderInsertReq->ContingentCondition = THOST_FTDC_CC_Immediately;
+		///强平原因: 非强平
+		orderInsertReq->ForceCloseReason = THOST_FTDC_FCC_NotForceClose;
+		///自动挂起标志: 否
+		orderInsertReq->IsAutoSuspend = 0;
+		///用户强评标志: 否
+		//orderInsertReq.UserForceClose = 0;
+	}
+
+	void makeClearLimitOrder(double lastPrice, TThostFtdcDirectionType direction, TThostFtdcOffsetFlagType offsetFlag, TThostFtdcVolumeType volume) {
+		std::shared_ptr<CThostFtdcInputOrderField> orderInsertReq = std::make_shared<CThostFtdcInputOrderField>();
+		memset(orderInsertReq.get(), 0, sizeof(CThostFtdcInputOrderField));
+		strcpy(orderInsertReq->InstrumentID, this->instrumentID.c_str());
+		orderInsertReq->Direction = direction;
+		orderInsertReq->CombOffsetFlag[0] = offsetFlag;
+		orderInsertReq->LimitPrice = lastPrice;
+		orderInsertReq->VolumeTotalOriginal = volume;
+		//orderInsertReq->StopPrice = 0;
+		///报单引用
+		//strcpy(orderInsertReq.OrderRef, order_ref);
+		///报单价格条件: 限价
+		orderInsertReq->OrderPriceType = THOST_FTDC_OPT_LimitPrice;
+		///组合投机套保标志
+		orderInsertReq->CombHedgeFlag[0] = THOST_FTDC_HF_Speculation;
+		///有效期类型: 当日有效
+		orderInsertReq->TimeCondition = THOST_FTDC_TC_IOC;
+		///成交量类型: 任何数量
+		orderInsertReq->VolumeCondition = THOST_FTDC_VC_AV;
+		///最小成交量: 1
+		orderInsertReq->MinVolume = 1;
+		///触发条件: 立即
+		orderInsertReq->ContingentCondition = THOST_FTDC_CC_Immediately;
+		///强平原因: 非强平
+		orderInsertReq->ForceCloseReason = THOST_FTDC_FCC_NotForceClose;
+		///自动挂起标志: 否
+		orderInsertReq->IsAutoSuspend = 0;
+		///用户强评标志: 否
+		//orderInsertReq.UserForceClose = 0;
+	}
+
+	void clearInvestor(CThostFtdcInvestorPositionField investor, int status, bool isLast);
+
+	void addCurVolume(TThostFtdcVolumeType v, TThostFtdcDirectionType direction, double p) {
+		std::lock_guard<std::mutex> lk(strategyMutex);
+		curVolume += v;
+		double ps = v * (p*InstrumentCommissionRate.OpenRatioByMoney *instrumentField.VolumeMultiple + InstrumentCommissionRate.OpenRatioByVolume);
+		double cost = -(v * p + ps);
+		CommissionFile << instrumentID << ","
+			<< v << ","
+			<< p << ","
+			<< ps << ","
+			<< THOST_FTDC_OF_CloseToday << ","
+			<< direction << ","
+			<< cost << std::endl;
+		if (status >= 8) {
+			status -= 8;
+		}
+
+		costArray.push_back(cost);
+	}
+
+	void subCurVolume(TThostFtdcVolumeType v, TThostFtdcDirectionType direction, double p) {
+		std::lock_guard<std::mutex> lk(strategyMutex);
+		curVolume -= v;
+		double ps = v * (p*InstrumentCommissionRate.CloseTodayRatioByMoney *instrumentField.VolumeMultiple + InstrumentCommissionRate.CloseTodayRatioByVolume);
+		double cost = v * p - ps;
+		CommissionFile << instrumentID << ","
+			<< v << ","
+			<< p << ","
+			<< ps << ","
+			<< THOST_FTDC_OF_CloseToday << ","
+			<< direction << ","
+			<< cost << std::endl;
+		costArray.push_back(cost);
+		if (curVolume > 0) {
+			makeClearOrder(0, direction, THOST_FTDC_OF_CloseToday, curVolume.load());
+		}
+		else if (curVolume == 0) {
+			double sum = 0;
+			for (double cs : costArray) {
+				sum += cs;
+			}
+
+			if (sum > 0) {
+				profit += sum;
+				if (loss != 0)
+					winFile << 1 << "," << profit / loss << std::endl;
+				else {
+					winFile << 1 << "," << "N/A" << std::endl;
+				}
+			}
+			else {
+				loss += (-sum);
+				if (loss != 0)
+					winFile << 0 << "," << profit / loss << std::endl;
+				else {
+					winFile << 0 << "," << "N/A" << std::endl;
+				}
+			}
+			costArray.clear();
+			status = 0;
+		}
+		else {
+			throw "error";
+		}
+	}
+
+	bool getOpStart() {
+		return opStart;
+	}
+
+	void setInstrumentField(const CThostFtdcInstrumentField& field) {
+		if (strcmp(field.InstrumentID, instrumentID.c_str()) == 0) {
+			instrumentField = field;
+		}
+	}
+
+	void setInstrumentCommissionRate(CThostFtdcInstrumentCommissionRateField commissionRate) {
+		InstrumentCommissionRate = commissionRate;
+	}
+	void clearStatus(int v) {
+		if (MvStatus == 1) {
+			initVolume -= v;
+			//customTradeSpi->reqQueryInvestorPosition();
+		}
+	}
+
+private:
+	std::ofstream outFile;
+	std::ofstream CommissionFile;
+	std::ofstream winFile;
+	std::mutex strategyMutex;
+	std::list<double> highPivotQue;
+	std::list<double> lowPivotQue;
+
+	CThostFtdcInvestorPositionField longInvestor;
+	CThostFtdcInvestorPositionField shortInvestor;
+	int preStatus = 0;
+	int status = 0; //0无单，1买多单， 2买空单
+	int left;
+	int right;
+	int barsNumHigh = 0;
+	int barsNumLow = 0;
+
+	int initVolume = 0;
+	bool last = false;
+	bool opStart = false;
+	int MvStatus = 0;
+	std::list<std::function<void()>> taskQue;
+	std::vector<std::thread> tasks;
+	std::vector<double> costArray;
+	double profit = 0;
+	double loss = 0;
+	std::atomic<int> curVolume = 0;
+	CThostFtdcInstrumentField instrumentField;
+	CThostFtdcInstrumentCommissionRateField InstrumentCommissionRate;
+	double pivot(Strategy::Type type);
+};
